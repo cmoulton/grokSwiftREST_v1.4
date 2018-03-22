20a564c72d46db8e1f9147fe324bd3c574969ec3
Christina Moulton <christina@teakmobile.com>
2018-03-22 15:07:40 -0400

    Initial Commit

---
+++ grokSwiftREST/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2018-03-22.
+//  Copyright © 2018 Christina Moulton. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = detailItem {
+        if let label = detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  var detailItem: NSDate? {
+    didSet {
+        // Update the view.
+        configureView()
+    }
+  }
+
+
+}
+
