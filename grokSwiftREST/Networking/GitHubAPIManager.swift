2308c8fe2d587000b73b37c28d07e2977121f154
Christina Moulton <christina@teakmobile.com>
2018-03-23 12:03:10 -0400

    Added other example basic auth functions

--- grokSwiftREST/Networking/GitHubAPIManager.swift
+++ grokSwiftREST/Networking/GitHubAPIManager.swift
@@ -24,6 +24,38 @@ class GitHubAPIManager {
     }
   }
 
+  func doGetWithBasicAuth() {
+    let username = "myUsername"
+    let password = "myPassword"
+    Alamofire.request("https://httpbin.org/basic-auth/\(username)/\(password)")
+      .authenticate(user: username, password: password)
+      .responseString { response in
+        if let receivedString = response.result.value {
+          print(receivedString)
+        } else if let error = response.result.error {
+          print(error)
+        }
+    }
+  }
+
+  func doGetWithBasicAuthCredential() {
+    let username = "myUsername"
+    let password = "myPassword"
+
+    let credential = URLCredential(user: username, password: password,
+                                   persistence: .forSession)
+
+    Alamofire.request("https://httpbin.org/basic-auth/\(username)/\(password)")
+      .authenticate(usingCredential: credential)
+      .responseString { response in
+        if let receivedString = response.result.value {
+          print(receivedString)
+        } else if let error = response.result.error {
+          print(error)
+        }
+    }
+  }
+
   func clearCache() {
     let cache = URLCache.shared
     cache.removeAllCachedResponses()
